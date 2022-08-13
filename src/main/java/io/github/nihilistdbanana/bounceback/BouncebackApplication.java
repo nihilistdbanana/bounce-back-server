13553c66a91d5c1ea5d73cb6d145e51e793fb4c8
banana <lbsrex@gmail.com>
2022-08-13 23:32:41 +0530

    Initialized the Project

---
+++ src/main/java/io/github/nihilistdbanana/bounceback/BouncebackApplication.java
@@ -0,0 +1,13 @@
+package io.github.nihilistdbanana.bounceback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BouncebackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BouncebackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/nihilistdbanana/bounceback/BouncebackApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.nihilistdbanana.bounceback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BouncebackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
